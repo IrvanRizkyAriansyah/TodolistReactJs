5275b2821760c40165cfae7cec723756d9fccd70
unknown <irvanrizky58@gmail.com>
2022-09-27 15:54:24 +0700

    search and checkbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import {Button, Row, Col, Input} from 'antd';
+import { Link, useNavigate } from "react-router-dom";
+import {Button, Row, Col, Input, Checkbox} from 'antd';
 import {DeleteFilled, EditFilled} from '@ant-design/icons';
 
 function Home() {
   const [toDo, setToDo] = useState([]);
   const [task, setTask] = useState([]);
+  const [searchTask, setSearchTask] = useState('');
+
+  const navigate = useNavigate();
 
   function loadTasks() {
     axios.get("http://localhost:3001/tasks").then((res) => {
       setToDo(res.data.reverse());
-      setTask(res.data.reverse());
+      setTask(res.data);
     });
   }
 
   useEffect(() => {
     loadTasks();
   }, []);
-
+  
   function deleteTask(id) {
-    axios.delete(`http://localhost:3001/tasks/${id}`).then(loadTasks());
+    axios.delete(`http://localhost:3001/tasks/${id}`)(loadTasks());
   }
   
   function filterTodoTask() {
@@ -34,19 +37,32 @@ function Home() {
   function filterAllTask() {
     setToDo(task)
   }
-
+  
+  const completeTask = (id) => {
+        setToDo(toDo.map((item) => {
+            if(item.id === id){
+                return {
+                    ...item, complete : !item.complete
+                }
+            } return item
+        }))
+    }  
+  
   return (
     <>
     <div className="container">
         <h1 align="center">TodoSearch</h1>
         <div className="box">
-          <Input placeholder="Search" style={{width: "50%"}}/>
+          <Input placeholder="Search" style={{width: "50%"}} 
+          onChange={e => {setSearchTask(e.target.value)}}
+          />
         <Row style={{paddingTop: "1rem"}}>
         <Col span={12}>
-          <Button type="primary" style={{width: "100%"}}>Search</Button>
+          <Button type="primary" style={{width: "100%"}}
+          >Search</Button>
         </Col>
         <Col span={12} align="end">
-          <Link to={"/add-task"}> <Button type="primary" style={{width: "80%"}} >Add new task</Button> </Link>
+          <Button type="primary" style={{width: "80%"}} onClick={()=>navigate('/add-task')} >Add new task</Button> 
         </Col>
         </Row>
         </div>
@@ -68,15 +84,27 @@ function Home() {
       </Col>
       </Row>
       
-        {toDo.map((data, index) => { return (
+        {toDo.filter((data) => {
+              if (searchTask ===''){
+                 return data
+              } else if (data.task.toLowerCase().includes(searchTask.toLowerCase())){
+                 return data
+              }
+              }).map((data, index) => { return (
           <div className="box">
             <Row>
             <Col span={12}> 
               <h3 className={data.complete?'done':''}> {data.task} </h3> 
             </Col>
             <Col span={12} align="end">
-              <Link to={`/edit-task/${data.id}`}> <EditFilled style={{color:"yellow", marginRight:"1rem", fontSize: "16pt"}} /> </Link>
-              <Link onClick={()=>deleteTask(data.id)} to={"#"}> <DeleteFilled style={{fontSize: "18pt"}} /> </Link>
+              <Checkbox checked={data.complete? true : false } style={{marginRight:"1rem"}}              
+              onClick={() => completeTask(data.id)} />
+              <EditFilled style={{color:"yellow", marginRight:"1rem", fontSize: "14pt"}} 
+                onClick={()=>navigate(`/edit-task/${data.id}`)}
+              /> 
+              <DeleteFilled style={{fontSize: "14pt"}} 
+                onClick={()=>deleteTask(data.id)}
+              />
             </Col>
             </Row>
          </div>
@@ -89,6 +117,7 @@ function Home() {
       </Col>
       <Col span={12} align="end">
         <Button type="primary" style={{width: "95%", backgroundColor: "red", border:"none"}} 
+        
         >Delete all task</Button>
       </Col>
       </Row>
